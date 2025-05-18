3a8215089998955fec9464efe33fbb2e97388ee2
Zinx140 <frostocean4@gmail.com>
2025-05-18 18:23:52 +0700

    asset dan GUI

--- project/src/Player.java
+++ project/src/Player.java
@@ -11,9 +11,10 @@ public class Player implements Cloneable {
         this.gp = gp;
     }
 
-    public int searchKey(ArrayList<Key> keys, Player player) {
+    public int searchKey(ArrayList<Plate> keys) {
+        System.out.println(keys.size());
         for (int i = 0; i < keys.size(); i++) {
-            if (player.playerX == keys.get(i).keyX && player.playerY == keys.get(i).keyY) {
+            if (playerX == keys.get(i).keyX && playerY == keys.get(i).keyY) {
                 return i; // Mengembalikan index kunci yang ditemukan
             }
         }
@@ -24,88 +25,89 @@ public class Player implements Cloneable {
         for (int i = 0; i < gp.MAX_WORLD_ROW; i++) {
             for (int j = 0; j < gp.MAX_WORLD_COL; j++) {
                 if (map[i][j] == 4) {
-                    map[i][j] = 6; // Set tile jalan
+                    map[i][j] = 6; 
                 }
             }
         }
     }
 
-    public void move(int map[][], int direction, ArrayList<Key> keys) {
+    public void move(int map[][], int direction, ArrayList<Plate> keys, boolean isSolving) {
+        int trace = (isSolving) ? 4 : 6; // Set trace tile based on solving state
         switch (direction) {
             case 0: // up
                 if (map[playerX][playerY - 1] == 2) {
                     playerY--;  
                 } else if (map[playerX][playerY - 1] == 5) { 
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerY--;
-                    map[playerX][playerY] = 3; // Set tile player
-                    int keyIndex = searchKey(keys, this);
+                    map[playerX][playerY] = 3; 
+                    int keyIndex = searchKey(keys);
                     if (keyIndex != -1) {
                         keys.get(keyIndex).openPath(map);
-                        keys.remove(keyIndex); // Menghapus kunci setelah digunakan
+                        keys.remove(keyIndex); 
                     }
                     clearTrace(map);
                 } else if (map[playerX][playerY - 1] != 1 && map[playerX][playerY - 1] != 4 && map[playerX][playerY - 1] != 2) {
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerY--;
-                    map[playerX][playerY] = 3; // Set tile player
+                    map[playerX][playerY] = 3; 
                 }
                 break;
             case 1: // down
                 if (map[playerX][playerY + 1] == 2) {
                     playerY++;
                 } else if (map[playerX][playerY + 1] == 5) { 
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerY++;
-                    map[playerX][playerY] = 3; // Set tile player
-                    int keyIndex = searchKey(keys, this);
+                    map[playerX][playerY] = 3; 
+                    int keyIndex = searchKey(keys);
                     if (keyIndex != -1) {
                         keys.get(keyIndex).openPath(map);
-                        keys.remove(keyIndex); // Menghapus kunci setelah digunakan
+                        keys.remove(keyIndex);
                     }
                     clearTrace(map);
                 } else if (map[playerX][playerY + 1] != 1 && map[playerX][playerY + 1] != 4 && map[playerX][playerY + 1] != 2) {
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerY++;
-                    map[playerX][playerY] = 3; // Set tile player
+                    map[playerX][playerY] = 3; 
                 }
                 break;
             case 2: // left
                 if (map[playerX - 1][playerY] == 2) {
                     playerX--;
                 } else if (map[playerX - 1][playerY] == 5) { 
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerX--;
-                    map[playerX][playerY] = 3; // Set tile player
-                    int keyIndex = searchKey(keys, this);
+                    map[playerX][playerY] = 3; 
+                    int keyIndex = searchKey(keys);
                     if (keyIndex != -1) {
                         keys.get(keyIndex).openPath(map);
-                        keys.remove(keyIndex); // Menghapus kunci setelah digunakan
+                        keys.remove(keyIndex); 
                     }
                     clearTrace(map);
                 } else if (map[playerX - 1][playerY] != 1 && map[playerX - 1][playerY] != 4 && map[playerX - 1][playerY] != 2) {
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerX--;
-                    map[playerX][playerY] = 3; // Set tile player
+                    map[playerX][playerY] = 3; 
                 }
                 break;
             case 3: // right
                 if (map[playerX + 1][playerY] == 2) {
                     playerX++;
                 } else if (map[playerX + 1][playerY] == 5) { 
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace;
                     playerX++;
-                    map[playerX][playerY] = 3; // Set tile players
-                    int keyIndex = searchKey(keys, this);
+                    map[playerX][playerY] = 3;
+                    int keyIndex = searchKey(keys);
                     if (keyIndex != -1) {
                         keys.get(keyIndex).openPath(map);
-                        keys.remove(keyIndex); // Menghapus kunci setelah digunakan
+                        keys.remove(keyIndex); 
                     }
                     clearTrace(map);
                 } else if (map[playerX + 1][playerY] != 1 && map[playerX + 1][playerY] != 4 && map[playerX + 1][playerY] != 2) {
-                    map[playerX][playerY] = 4; // Set tile jalan
+                    map[playerX][playerY] = trace; 
                     playerX++;
-                    map[playerX][playerY] = 3; // Set tile player
+                    map[playerX][playerY] = 3;
                 }
                 break;
         }
---
+++ project/src/TileManager.java
@@ -0,0 +1,139 @@
+import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+public class TileManager {
+    GamePanel gp;
+    Tile[] tile;
+    int[][] mapTile;
+
+    public TileManager(GamePanel gp) {
+        this.gp= gp;
+        tile = new Tile[30];
+        mapTile = new int[gp.MAX_WORLD_COL][gp.MAX_WORLD_ROW];
+
+        loadMap("Maze-With-Solver/project/src/map1.txt");
+        getTileImage();
+    }
+
+    public void getTileImage() {
+        try {
+            
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(new File("Maze-With-Solver/project/img/way.png"));
+            tile[0].collison = false;
+            
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(new File("Maze-With-Solver/project/img/wall.png"));
+            tile[1].collison = false;
+
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(new File("Maze-With-Solver/project/img/princess.png"));
+            tile[2].collison = false;
+            
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(new File("Maze-With-Solver/project/img/prince.png"));
+            tile[3].collison = false;
+            
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(new File("Maze-With-Solver/project/img/path.png"));
+            tile[4].collison = false;
+            
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(new File("Maze-With-Solver/project/img/plate.png"));
+            tile[5].collison = false;
+            
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(new File("Maze-With-Solver/project/img/path.png"));
+            tile[6].collison = false;
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    public void loadMap(String path) {
+        try {
+            File file = new File(path);
+            if (!file.exists()) {
+                System.err.println("File tidak ditemukan: " + path);
+                return;
+            }
+
+            InputStream is = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            List<Integer> numbers = new ArrayList<>();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.trim().split(" ");
+                for (String token : tokens) {
+                    if (!token.trim().isEmpty()) { // Pastikan token tidak kosong
+                        numbers.add(Integer.parseInt(token.trim()));
+                    }
+                }
+            }
+
+            br.close();
+
+            int totalTiles = gp.MAX_WORLD_COL * gp.MAX_WORLD_COL;
+            if (numbers.size() != totalTiles) {
+                throw new IllegalArgumentException("Jumlah angka dalam file tidak sesuai dengan ukuran peta (" 
+                    + gp.MAX_WORLD_COL + "x" + gp.MAX_WORLD_COL + "). Dibutuhkan " + totalTiles + " angka, tetapi ditemukan " + numbers.size() + ".");
+            }
+
+            int col = 0;
+            int row = 0;
+            for (int num : numbers) {
+                mapTile[col][row] = num;
+                col++;
+                if (col == gp.MAX_WORLD_COL) {
+                    col = 0;
+                    row++;
+                }
+            }
+
+            System.out.println("Peta berhasil dimuat!");
+
+        } catch (IOException e) {
+            System.err.println("Terjadi kesalahan saat membaca file: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+
+        int worldCol = 0;
+        int worldRow = 0;
+        while (worldCol < gp.MAX_WORLD_COL && worldRow < gp.MAX_WORLD_ROW) {
+
+            int tileNum = mapTile[worldCol][worldRow];
+
+            int worldX = worldCol * gp.TILE_SIZE;
+            int worldY = worldRow * gp.TILE_SIZE + 205;
+
+            if (tile[tileNum].image != null) {
+                g2.drawImage(tile[tileNum].image, worldX, worldY, gp.TILE_SIZE, gp.TILE_SIZE, null);
+            } else {
+                System.err.println("Gambar tile tidak ditemukan untuk tile number: " + tileNum);
+            }
+            
+            worldCol++;
+            
+            if (worldCol == gp.MAX_WORLD_COL) {
+                worldCol = 0;
+                worldRow++;
+            }
+        }
+    }
+}
